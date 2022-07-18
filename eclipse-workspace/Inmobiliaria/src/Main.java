12c9ae841289d5ad0a0e1428cd44e0389dc249b6
Fran <alejandrovelez1234567@gmail.com>
2022-07-18 20:52:48 +0200

    Version 1 Commit

---
+++ eclipse-workspace/Inmobiliaria/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+	
+	
+	public static void main(String args[]) {
+		
+		Solares solares = new Solares("Urb. Princialba", 35.0, 3.5, "URBANO");
+		Garajes garajes = new Garajes("Urb. Princialba", 15.0, 4.0, "PRIVADO");
+		
+		System.out.println(solares.toString());
+		System.out.println(garajes.toString());
+		
+	}
+}
